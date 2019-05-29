d6a392225deeedc8a1992a924134662d86189d62
wulman16 <will.ulman@gmail.com>
2019-05-29 15:18:12 -0400

    set up basic styling

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(express.static(publicDirectoryPath));
 
 app.get(``, (req, res) => {
   res.render(`index`, {
-    title: `Weather App`,
+    title: `Weather`,
     name: `Will Ulman`
   });
 });
