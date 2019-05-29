db6def1d59a688ba3f1aef6c1d1607e828f5f6a9
wulman16 <will.ulman@gmail.com>
2019-05-29 11:45:14 -0400

    integrate prettier

--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
-const path = require(`path`)
-const express = require(`express`)
+const path = require(`path`);
+const express = require(`express`);
 
-const publicDirectoryPath = path.join(__dirname, `../public`)
+const publicDirectoryPath = path.join(__dirname, `../public`);
 
-const app = express()
+const app = express();
 
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
 app.get(``, (req, res) => {
-  res.send(`<h1>Weather</h1>`)
-})
+  res.send(`<h1>Weather</h1>`);
+});
 
 app.get(`/weather`, (req, res) => {
   res.send({
     forecast: `Rain!`,
     location: `Atlanta`
-  })
-})
+  });
+});
 
 app.listen(3002, () => {
-  console.log(`Server is up on port 3002!`)
-})
+  console.log(`Server is up on port 3002!`);
+});
