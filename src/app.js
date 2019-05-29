4efd3f88f72e7146683d191b5c55b1650fd816ef
wulman16 <will.ulman@gmail.com>
2019-05-29 13:44:10 -0400

    initialize handlebars templates

--- src/app.js
+++ src/app.js
@@ -1,14 +1,30 @@
 const path = require(`path`);
 const express = require(`express`);
 
+const app = express();
 const publicDirectoryPath = path.join(__dirname, `../public`);
 
-const app = express();
-
+app.set(`view engine`, `hbs`);
 app.use(express.static(publicDirectoryPath));
 
 app.get(``, (req, res) => {
-  res.send(`<h1>Weather</h1>`);
+  res.render(`index`, {
+    title: `Weather App`,
+    name: `Will Ulman`
+  });
+});
+
+app.get(`/about`, (req, res) => {
+  res.render(`about`, {
+    title: `About`,
+    name: `Will Ulman`
+  });
+});
+
+app.get(`/help`, (req, res) => {
+  res.render(`help`, {
+    message: `Help page`
+  });
 });
 
 app.get(`/weather`, (req, res) => {
