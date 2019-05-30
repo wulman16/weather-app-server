c2226499a28e715a26b7ff0555e56fa50f0f4bef
wulman16 <will.ulman@gmail.com>
2019-05-30 15:30:02 -0400

    set up client-side fetching

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,10 @@
-console.log(`Client-side JS file is loaded!`);
+fetch(`http://localhost:3002/weather/?location=atlanta`)
+  .then(res => res.json())
+  .then(data => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      console.log(data.location);
+      console.log(data.forecast);
+    }
+  });
